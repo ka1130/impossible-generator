6ef8ac96cb81964d3c8fd468a2e16cc4f43d50e9
Kamila Tomczyk <ka1130@o2.pl>
2017-01-16 19:40:16 +0100

    Visually stable desktop version. Add upload photo functionality.

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
 
 // Upload a photo
-// function readURL(input) {
-//         if (input.files && input.files[0]) {
-//             var reader = new FileReader();
+function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
             
-//             reader.onload = function (e) {
-//                 $('#blah').attr('src', e.target.result);
-//             }
+            reader.onload = function (e) {
+                $('#userPhoto').attr('src', e.target.result);
+            }
             
-//             reader.readAsDataURL(input.files[0]);
-//         }
-//     }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
     
-//     $("#imgInp").change(function(){
-//         readURL(this);
-//     });
+    $("#uploadPhotoBtn").change(function(){
+        readURL(this);
+    });
 
 
 });
