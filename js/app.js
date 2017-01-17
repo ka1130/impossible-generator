1eb99f8f37ab83affdda69b65958dff5b286b494
Kamila Tomczyk <ka1130@o2.pl>
2017-01-17 21:55:01 +0100

    Zoom working, quality issue to be worked on.

--- js/app.js
+++ js/app.js
@@ -28,7 +28,9 @@ $(document).ready(function() {
 	var canvas = document.getElementById("imgCanvas");
 	var originalPhoto = document.getElementById("userPhoto");
 	var grayscaleRangeSlider = $("#grayscaleRange");
-	var resetBtn = $("#resetBtn");	
+	var brightenRangeSlider = $("#brightenRange");
+	var blurRangeSlider = $("#blurRange");
+	var resetBtn = $("#resetBtn");
 	var context = canvas.getContext("2d");
 
 	function drawImage(imageObj) {
@@ -37,10 +39,12 @@ $(document).ready(function() {
 		var y = 0;
 		var grayscaleVal = grayscaleRangeSlider.val() * 0.01;
 
-		context.drawImage(imageObj, x, y);
+		context.drawImage(imageObj, x, y); //namaluj mi nowy obraz na jakimś x i jakimś y
 
 		var imageData = context.getImageData(x, y, imageObj.width, imageObj.height);
+		//pobierz pozycję i wymiary z kontekstu
 		var data = imageData.data;
+		//zapisz te dane do zmiennej data, imageData.data przechowuje wartości poszczególnych pikseli (0-255)
 
 		for (var i = 0; i < data.length; i += 4) {
 			var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
@@ -53,7 +57,7 @@ $(document).ready(function() {
 		}
 
 		// overwrite original image
-		context.putImageData(imageData, x, y);
+		context.putImageData(imageData, x, y); //przetworzone dane umieść z powrotem na danej pozycji x i y
 
 	}
 
@@ -74,6 +78,33 @@ $(document).ready(function() {
 		context.drawImage(originalPhoto, 0, 0);
 	});
 
+	//przycinanie obrazu
+
+	// window.onload(function() {
+
+	// 	context.beginPath();
+	// 	context.rect(canvas.width / 2, canvas.height / 2, 187, 251);
+	// 	context.clip();
+	// 	context.scale(2, 2);
+	// 	context.drawImage(userPhoto);
+
+
+	// });
+
+	//Zoom-in, Zoom-out
+	var zoomIn = document.getElementById("zoom-in");
+	var zoomOut = document.getElementById("zoom-out");
+
+	zoomIn.addEventListener("click", function(event) {
+		context.scale(0.99, 0.99);
+		context.drawImage(originalPhoto, 0, 0);
+
+	}, false);
+
+	zoomOut.addEventListener("click", function(event) {
+		context.scale(1.01, 1.01);
+		context.drawImage(originalPhoto, 0, 0);
+	});
 
 
 
