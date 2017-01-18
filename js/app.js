576ad6a365107d34309e51e1c05de099b4d6ccfd
Kamila Tomczyk <ka1130@o2.pl>
2017-01-18 12:49:06 +0100

    Corrected positioning and zoom.

--- js/app.js
+++ js/app.js
@@ -1,166 +1,200 @@
   document.addEventListener("DOMContentLoaded", function(event) {
 
-    console.log("DOM fully loaded and parsed"); 
+  	console.log("DOM fully loaded and parsed");
 
-	// Upload a photo
+  	// Upload a photo
 
-	var uploadPhoto = document.getElementById("uploadPhotoBtn");
-	var userPhoto = document.getElementById("userPhoto");
-	var imageObj = new Image();
+  	var uploadPhoto = document.getElementById("uploadPhotoBtn");
+  	var userPhoto = document.getElementById("userPhoto");
+  	//	var imageObj = new Image();
 
-	var grayscaleBtn = document.getElementsByTagName("button")[0];
-	var canvas = document.getElementById("imgCanvas");
-	var originalPhoto = document.getElementById("userPhoto");
-	var grayscaleRangeSlider = document.getElementById("grayscaleRange");
-	var brightenRangeSlider = document.getElementById("brightenRange");
-	var blurRangeSlider = document.getElementById("blurRange");
-	var resetBtn = document.getElementById("resetBtn");
-	var ctx = canvas.getContext("2d");
+  	var grayscaleBtn = document.getElementsByTagName("button")[0];
+  	var canvas = document.getElementById("imgCanvas");
+  	var originalPhoto = document.getElementById("userPhoto");
+  	var grayscaleRangeSlider = document.getElementById("grayscaleRange");
+  	var brightenRangeSlider = document.getElementById("brightenRange");
+  	var blurRangeSlider = document.getElementById("blurRange");
+  	var resetBtn = document.getElementById("resetBtn");
+  	var ctx = canvas.getContext("2d");
 
-	var moveTop = document.getElementById("moveTop");
-	var moveRight = document.getElementById("moveRight");
-	var moveLeft = document.getElementById("moveLeft");
-	var moveBottom = document.getElementById("moveBottom");
+  	var moveTop = document.getElementById("moveTop");
+  	var moveRight = document.getElementById("moveRight");
+  	var moveLeft = document.getElementById("moveLeft");
+  	var moveBottom = document.getElementById("moveBottom");
 
-	var zoomIn = document.getElementById("zoom-in");
-	var zoomOut = document.getElementById("zoom-out");
+  	var zoomIn = document.getElementById("zoom-in");
+  	var zoomOut = document.getElementById("zoom-out");
 
-	var rotateControl = document.querySelector(".rotation-circle");
+  	var rotateControl = document.querySelector(".rotation-circle");
+  	var deg = 1;
+  	var x;
+  	var y;
 
-	var x = 0;
-	var y = 0;
-	var deg = 1;
+  	if (typeof x === "undefined") {
+  		x = 0;
+  	}
 
-	function readURL(input) {
-		if (input.files && input.files[0]) {
-			var reader = new FileReader();
+  	if (typeof y === "undefined") {
+  		y = 0;
+  	}
 
-			reader.onload = function(event) {
-				userPhoto.setAttribute("src", event.target.result);
-			}
+  	function readURL(input) {
+  		if (input.files && input.files[0]) {
+  			var reader = new FileReader();
 
-			reader.readAsDataURL(input.files[0]);
-		}
-	}
+  			reader.onload = function(event) {
+  				userPhoto.setAttribute("src", event.target.result);
+  			}
 
-	uploadPhoto.addEventListener("change", function(event) {
-		readURL(this);
-	}, false);
+  			reader.readAsDataURL(input.files[0]);
+  		}
+  	}
 
-
-	//Filters - Grayscale
-
-	function drawImage(imageObj) {
-
-		var grayscaleVal = grayscaleRangeSlider.value * 0.01;
-
-		ctx.drawImage(imageObj, x, y); //namaluj mi nowy obraz na jakimś x i jakimś y
-
-		var imageData = ctx.getImageData(x, y, imageObj.width, imageObj.height);
-		//pobierz pozycję i wymiary z kontekstu
-		var data = imageData.data;
-		//zapisz te dane do zmiennej data, imageData.data przechowuje wartości poszczególnych pikseli (0-255)
-
-		for (var i = 0; i < data.length; i += 4) {
-			var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
-			// red
-			data[i] = brightness / grayscaleVal;
-			// green
-			data[i + 1] = brightness / grayscaleVal;
-			// blue
-			data[i + 2] = brightness / grayscaleVal;
-		}
-		// overwrite original image
-		ctx.putImageData(imageData, x, y); //przetworzone dane umieść z powrotem na danej pozycji x i y
-
-	}
-
-	grayscaleRangeSlider.addEventListener("input", function(event) {
-
-		var imgContainer = document.querySelector("#imgContainer");
-		var imgList = imgContainer.getElementsByTagName("img");
-		var form1 = document.querySelector("#form1");
-		var imgHide = form1.getElementsByTagName("img")[0];
-
-		if (imgList.length == 1) {
-			canvas.parentNode.insertBefore(originalPhoto.cloneNode(true), canvas);
-		}
-
-		imgHide.style.display = "none";
-		drawImage(userPhoto);
-
-	}, false);
-
-	resetBtn.addEventListener("click", function(event) {
-		ctx.drawImage(originalPhoto, 0, 0, canvas.width, canvas.height);
-	}, false);
-
-	//przycinanie obrazu
-
-	// window.onload(function() {
-
-	// 	ctx.beginPath();
-	// 	ctx.rect(canvas.width / 2, canvas.height / 2, 187, 251);
-	// 	ctx.clip();
-	// 	ctx.scale(2, 2);
-	// 	ctx.drawImage(userPhoto);
+  	uploadPhoto.addEventListener("change", function(event) {
+  		readURL(this);
+  	}, false);
 
 
-	// });
+  	//Filters - Grayscale
 
-	//Zoom-in, Zoom-out
+  	function drawImage(imageObj) {
 
-	zoomIn.addEventListener("click", function(event) {
-		ctx.scale(0.99, 0.99);
-		ctx.drawImage(originalPhoto, 0, 0);
-	}, false);
+  		var grayscaleVal = grayscaleRangeSlider.value * 0.01;
 
-	zoomOut.addEventListener("click", function(event) {
-		ctx.scale(1.01, 1.01);
-		ctx.drawImage(originalPhoto, 0, 0);
-	}, false);
+  		ctx.drawImage(originalPhoto, x, y); //namaluj mi nowy obraz na jakimś x i jakimś y
+
+  		var imageData = ctx.getImageData(x, y, imageObj.width, imageObj.height);
+  		//pobierz pozycję i wymiary z kontekstu
+  		var data = imageData.data;
+  		//zapisz te dane do zmiennej data, imageData.data przechowuje wartości poszczególnych pikseli (0-255)
+
+  		for (var i = 0; i < data.length; i += 4) {
+  			var brightness = 0.34 * data[i] + 0.5 * data[i + 1] + 0.16 * data[i + 2];
+  			// red
+  			data[i] = brightness / grayscaleVal;
+  			// green
+  			data[i + 1] = brightness / grayscaleVal;
+  			// blue
+  			data[i + 2] = brightness / grayscaleVal;
+  		}
+  		// overwrite original image
+  		ctx.putImageData(imageData, x, y); //przetworzone dane umieść z powrotem na danej pozycji x i y
+
+  	}
+
+  	grayscaleRangeSlider.addEventListener("input", function(event) {
+
+  		var imgContainer = document.querySelector("#imgContainer");
+  		var imgList = imgContainer.getElementsByTagName("img");
+  		var form1 = document.querySelector("#form1");
+  		var imgHideList = form1.getElementsByTagName("img");
+
+  		if (imgList.length == 1) {
+  			canvas.parentNode.insertBefore(originalPhoto.cloneNode(true), canvas);
+  		}
+
+  		imgHideList[0].style.display = "none";
+  		drawImage(userPhoto);
+
+  	}, false);
+
+  	resetBtn.addEventListener("click", function(event) {
+  		ctx.drawImage(originalPhoto, 0, 0, canvas.width, canvas.height);
+  	}, false);
+
+  	//przycinanie obrazu
+
+  	// window.onload(function() {
+
+  	// 	ctx.beginPath();
+  	// 	ctx.rect(canvas.width / 2, canvas.height / 2, 187, 251);
+  	// 	ctx.clip();
+  	// 	ctx.scale(2, 2);
+  	// 	ctx.drawImage(userPhoto);
 
 
-	//Move
+  	// });
 
-	moveTop.addEventListener("click", function(event) {
-		ctx.drawImage(originalPhoto, x, y);
-		y -= 2;
-	}, false);
+  	//Zoom-in, Zoom-out
 
-	moveRight.addEventListener("click", function(event) {
-		ctx.drawImage(originalPhoto, x, y);
-		x -= 2;
-	}, false);
+  	zoomIn.addEventListener("click", function(event) {
+
+  		if (x === 0) {
+  			x = 0.99;
+  		}
+
+  		if (y === 0) {
+  			y = 0.99;
+  		}
+
+  		ctx.drawImage(originalPhoto, x, y);
+  		ctx.scale(x, y);
+
+  		x *= 0.99;
+  		y *= 0.99;
+
+  		console.log(x, y);
+  	}, false);
+
+  	zoomOut.addEventListener("click", function(event) {
+  		if (x === 0) {
+  			x = 1.11;
+  		}
+
+  		if (y === 0) {
+  			y = 1.11;
+  		}
+
+  		ctx.drawImage(originalPhoto, x, y);
+  		ctx.scale(x, y);
+
+  		x *= 1.11;
+  		y *= 1.11;
+  		
+  		console.log(x, y);
+  	}, false);
 
 
-	moveLeft.addEventListener("click", function(event) {
-		ctx.drawImage(originalPhoto, x, y);
-		x += 2;
-	}, false);
+  	//Move
 
-	moveBottom.addEventListener("click", function(event) {
-		ctx.drawImage(originalPhoto, x, y);
-		y += 2;
-	}, false);
+  	moveTop.addEventListener("click", function(event) {
+  		ctx.drawImage(originalPhoto, x, y);
+  		y -= 2;
+  	}, false);
 
-
-	//Rotation
-
-	rotateControl.addEventListener("click", function(event) {
-		if (deg == 1) {
-			deg -= 16;
-		} else {
-			deg -= 15;
-		}
-		ctx.save();
-		ctx.translate(93.5, 125.5);
-		ctx.rotate(deg * Math.PI / 180);
-		ctx.translate(-93.5, -125.5);
-		ctx.drawImage(originalPhoto, 0, 0);
-		ctx.restore();
-	}, false);
+  	moveRight.addEventListener("click", function(event) {
+  		ctx.drawImage(originalPhoto, x, y);
+  		x += 2;
+  	}, false);
 
 
+  	moveLeft.addEventListener("click", function(event) {
+  		ctx.drawImage(originalPhoto, x, y);
+  		x -= 2;
+  	}, false);
 
-});
+  	moveBottom.addEventListener("click", function(event) {
+  		ctx.drawImage(originalPhoto, x, y);
+  		y += 2;
+  	}, false);
+
+
+  	//Rotation
+
+  	rotateControl.addEventListener("click", function(event) {
+  		if (deg == 1) {
+  			deg -= 16;
+  		} else {
+  			deg -= 15;
+  		}
+  		ctx.save();
+  		ctx.translate(93.5, 125.5);
+  		ctx.rotate(deg * Math.PI / 180);
+  		ctx.translate(-93.5, -125.5);
+  		ctx.drawImage(originalPhoto, 0, 0);
+  		ctx.restore();
+  	}, false);
+
+
+
+  });
