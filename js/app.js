e2c78d110c87a38d0c2d40c9f44db1822c3099de
Kamila Tomczyk <ka1130@o2.pl>
2017-01-24 15:18:31 +0100

    Left-hand tools all functioning.

--- js/app.js
+++ js/app.js
@@ -51,8 +51,11 @@
 
     var zoomIn = document.getElementById("zoom-in");
     var zoomOut = document.getElementById("zoom-out");
+    var toolsLeftZoomIn = document.getElementById("toolsLeftZoomIn");
+    var toolsLeftZoomOut = document.getElementById("toolsLeftZoomOut");
 
     var rotateControl = document.querySelector(".rotation-circle");
+    var toolsLeftRotate = document.getElementById("toolsLeftRotate");
 
     var x;
     var y;
@@ -310,55 +313,74 @@
       ctx.drawImage(userPhoto, a, b);
     }
 
-    moveTop.addEventListener("click", moveElementTop, false);
-    toolsLeftMoveTop.addEventListener("click", moveElementTop, false);
-
-    moveRight.addEventListener("click", function(event) {
-      clearImage();
-      ctx.translate(a += 2, b);
-      ctx.drawImage(userPhoto, a, b);
-    }, false);
-
-    moveLeft.addEventListener("click", function(event) {
+    function moveElementLeft(event) {
       clearImage();
       ctx.translate(a -= 2, b);
       ctx.drawImage(userPhoto, a, b);
-    }, false);
+    }
 
-    moveBottom.addEventListener("click", function(event) {
+    function moveElementRight(event) {
+      clearImage();
+      ctx.translate(a += 2, b);
+      ctx.drawImage(userPhoto, a, b);
+    }
+
+    function moveElementBottom(event) {
       clearImage();
       ctx.translate(a, b += 2);
       ctx.drawImage(userPhoto, a, b);
-    }, false);
+    }
+
+    moveTop.addEventListener("click", moveElementTop, false);
+    toolsLeftMoveTop.addEventListener("click", moveElementTop, false);
+
+    moveLeft.addEventListener("click", moveElementLeft, false);
+    toolsLeftMoveLeft.addEventListener("click", moveElementLeft, false);
+
+    moveRight.addEventListener("click", moveElementRight, false);
+    toolsLeftMoveRight.addEventListener("click", moveElementRight, false);
+
+    moveBottom.addEventListener("click", moveElementBottom, false);
+    toolsLeftMoveBottom.addEventListener("click", moveElementBottom, false);
 
 
     // Zoom-in, Zoom-out
 
-    zoomIn.addEventListener("click", function(event) {
+    function zoomInElement(event) {
       ctx.scale(0.99, 0.99);
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.drawImage(userPhoto, x, y);
       userPhoto.style.display = "none";
-    }, false);
+    }
 
-    zoomOut.addEventListener("click", function(event) {
+    function zoomOutElement(event) {
       ctx.scale(1.01, 1.01);
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.drawImage(userPhoto, x, y);
       userPhoto.style.display = "none";
-    }, false);
+    }
+
+    zoomIn.addEventListener("click", zoomInElement, false);
+    toolsLeftZoomIn.addEventListener("click", zoomInElement, false);
+
+    zoomOut.addEventListener("click", zoomOutElement, false);
+    toolsLeftZoomOut.addEventListener("click", zoomOutElement, false);
 
 
     // Rotation
 
-    rotateControl.addEventListener("click", function(event) {
+    function rotateElement(event) {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.translate(93.5, 125.5); //rotating around the middle point of the photo
       ctx.rotate(15 * Math.PI / 180); //by 15deg
       ctx.translate(-93.5, -125.5); //rotating around the middle point of the photo
       ctx.drawImage(userPhoto, x, y);
       userPhoto.style.display = "none";
-    }, false);
+    }
+
+    rotateControl.addEventListener("click", rotateElement, false);
+    toolsLeftRotate.addEventListener("click", rotateElement, false);
+
 
 
     // Filters: Grayscale
